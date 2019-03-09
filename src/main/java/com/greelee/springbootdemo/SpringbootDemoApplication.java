845908e45152fa6f099d63c5cba7772a0967d5c2
gelin <gllovejava@163.com>
2019-03-09 20:31:47 +0800

    first commit

---
+++ src/main/java/com/greelee/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.greelee.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
